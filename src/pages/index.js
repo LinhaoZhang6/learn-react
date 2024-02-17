0f586eb98660f1a9dff25b1e3552348d1bb06730
LinhaoZhang6 <zhang.linha@northeastern.edu>
2024-02-17 16:27:54 -0500

    finished class activity

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Form from './components/updObjects'
+import Scoreboard from './qcomps/updObjectsForm'
+
+
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Scoreboard />
     </div>
   )
 }
